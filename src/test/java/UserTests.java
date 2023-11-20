04f5e417cc470c942a7ae88332389f2e2479221a
Amalia Padureanu <79747055+AmaliaPadureanu@users.noreply.github.com>
2023-11-20 20:09:06 +0200

    Adds User tests

--- src/test/java/UserTests.java
+++ src/test/java/UserTests.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class UserTests {
+import Models.User;
+import config.PetStoreEndpoints;
+import config.TestConfig;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class UserTests extends TestConfig {
+
+    @Test
+    public void login() {
+
+        given()
+                .queryParam("username", "test")
+                .queryParam("password", "abc123")
+        .when()
+                .get(PetStoreEndpoints.LOGIN)
+        .then();
+    }
+
+    @Test
+    public void createUser() {
+        User newUser = new User(1919, "BabyShark", "Baby", "Shark",
+                "bshark@gmail.com", "blabla123", "399948394", 0);
+
+        given()
+                .body(newUser)
+        .when()
+                .post(PetStoreEndpoints.CREATE_USER)
+        .then();
+    }
+
+    @Test
+    public void getUserById() {
+        given()
+                .pathParam("username", "BabyShark")
+                .when()
+                .get(PetStoreEndpoints.USER_BY_NAME)
+                .then();
+
+    }
 }
